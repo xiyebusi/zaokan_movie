e49317c7d5225e2af9372397c9b6139c224b574b
xiyebusi <xiyebusi@163.com>
2018-10-17 23:12:18 +0800

    10172312

---
+++ pages/info/info.js
@@ -0,0 +1,85 @@
+// pages/info/info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    avatar: 'http://a.hiphotos.baidu.com/zhidao/wh%3D450%2C600/sign=0179844a868ba61edfbbc02b7404bb3c/64380cd7912397dd11081a845d82b2b7d0a28739.jpg',
+    nickname: '我叫赵日天',
+    recordNumber: 123,
+    watchNumber: 0,
+    favouriteNumber: 0,
+    recordList: [{
+      post: 'http://wx4.sinaimg.cn/mw690/80df6fe6gy1fw1qpdnrs2j20u018g7c2.jpg',
+      name: '毒液：致命守护者 Venom',
+      src:''
+    }, 
+    {
+      post: 'http://wx4.sinaimg.cn/mw690/80df6fe6ly1fwamw7hkxvj205006k0ta.jpg',
+      name: '我要静静',
+      src: ''
+      },
+      {
+        post: 'http://wx4.sinaimg.cn/mw690/80df6fe6ly1fwamw4lqn4j205006kdg7.jpg',
+        name: '狄仁杰之蚩尤血藤',
+        src: ''
+      }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
