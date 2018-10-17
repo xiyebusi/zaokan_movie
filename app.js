b4a1b2dfbce6dc2b8ba41bc074406c1e6a65720d
xiyebusi <xiyebusi@163.com>
2018-10-17 23:16:52 +0800

    10172316

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 App({
   onLaunch: function () {
     //微信登陆
-    wx.login({
-      success: function (res) {
-        var code = res.code;
-        if (code) {
-          console.log('获取用户登录凭证：' + code);
-        } else {
-          console.log('获取用户登录态失败：' + res.errMsg);
-        }
-      }
-    });
+    // wx.login({
+    //   success: function (res) {
+    //     var code = res.code;
+    //     if (code) {
+    //       console.log('获取用户登录凭证：' + code);
+    //     } else {
+    //       console.log('获取用户登录态失败：' + res.errMsg);
+    //     }
+    //   }
+    // });
 
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
